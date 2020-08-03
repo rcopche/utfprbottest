c201cbd5acdd3d57cdfa34db256de7926635d205
RUBENS <rubens@grupotcm.com.br>
2020-08-03 17:21:59 -0300

    Trocando a conexão para o heroku

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
 const express = require("express");
 const mysql = require("mysql");
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
 
 const app = express();
 
+
 const PORT = process.env.PORT || 8080;
 
-const connection = mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    user: "root",
-    database:"testing1"
-});
+//const connection = mysql.createConnection({
+//    host:"localhost",
+//    port:3306,
+//    user: "root",
+//   database:"testing1"
+//});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-connection.connect(function(error){
-    console.log("conectado ao banco");  
+client.connect(function(error){
+   console.log("conectado ao banco");  
     app.listen(PORT, function(){
-        console.log("Escutando porta "+ PORT);
+       console.log("Escutando porta "+ PORT);
     })  
 });
 
 app.get("/api/all", function(req,res){
-    connection.query("SELECT * FROM TESTE", "testing1", function(error, result){
+    client.query("SELECT * FROM test1",  function(error, result){
         res.json(result);
     });    
 });
